998628a7a9d7489153cefccc91ac148552d41b3d
vicenzo-cesconetto <vicenzoccesconetto@gmail.com>
2018-04-09 17:56:02 -0300

    added the MusicCmdLine

--- MiniMusicApp/src/MiniMusicApp.java
+++ MiniMusicApp/src/MiniMusicApp.java
@@ -5,9 +5,10 @@ public class MiniMusicApp {
 
         MiniMusicApp mini = new MiniMusicApp();
         mini.play();
+        System.out.println("this is the end of main");
     }
 
-    public void play() {
+    private void play() {
         try {
             Sequencer player = MidiSystem.getSequencer();
 //            System.out.println("\nit did");
@@ -17,22 +18,30 @@ public class MiniMusicApp {
 
             Track track = seq.createTrack();
 
+//            ShortMessage first = new ShortMessage();
+//            first.setMessage(192, 1, 102, 0);
+//            MidiEvent change = new MidiEvent(first, 1);
+//            track.add(change);
+
             ShortMessage a = new ShortMessage();
-            a.setMessage(144, 1, 44, 100);
+            a.setMessage(144, 6, 44, 125);
             MidiEvent noteOn = new MidiEvent(a, 1);
             track.add(noteOn);
 
             ShortMessage b = new ShortMessage();
-            b.setMessage(128, 1, 44, 100);
+            b.setMessage(128, 6, 44, 125);
             MidiEvent noteOff = new MidiEvent(b, 16);
             track.add(noteOff);
 
             player.setSequence(seq);
 
             player.start();
+//            player.close(); /*This doesnt work! But I know why...*/
 
         } catch (Exception ex) {
             ex.printStackTrace();
         }
+
+        System.out.println("it gets here");
     }
 }
